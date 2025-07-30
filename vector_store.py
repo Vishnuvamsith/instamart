f4bfc3bde85ce9225e36a79fc13c5bb8d53a3bc6
Vishnu Vamsith Yejju <yejju.v_int@external.swiggy.in>
2025-07-30 15:14:07 +0530

    updated changes

--- vector_store.py
+++ vector_store.py
@@ -149,7 +149,8 @@ from langchain_google_genai import GoogleGenerativeAIEmbeddings
 from s3_manager import s3_manager
 from io import BytesIO
 import os
-
+from dotenv import load_dotenv
+load_dotenv()
 class VectorStoreManager:
     def __init__(self):
         self.embeddings = GoogleGenerativeAIEmbeddings(model=Config.EMBEDDING_MODEL)
