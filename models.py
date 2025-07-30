01fb33748fbc49df9eceeb295123dceca23e5910
Vishnu Vamsith Yejju <yejju.v_int@external.swiggy.in>
2025-07-30 14:56:03 +0530

    updated vector_store.py

--- models.py
+++ models.py
@@ -1,6 +1,6 @@
-from mongoengine import Document, StringField, BinaryField
+# from mongoengine import Document, StringField, BinaryField
 
-class PdfFile(Document):
-    filename = StringField(required=True, unique=True)
-    content_type = StringField(required=True)
-    data = BinaryField(required=True)
+# class PdfFile(Document):
+#     filename = StringField(required=True, unique=True)
+#     content_type = StringField(required=True)
+#     data = BinaryField(required=True)
